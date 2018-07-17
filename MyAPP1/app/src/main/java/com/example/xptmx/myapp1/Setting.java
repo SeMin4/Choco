32788fb364092df3f48e227291424820fef93ba1
SeMin <ohsemin4@gmail.com>
2018-07-17 21:24:04 +0900

    add Layout

---
+++ MyAPP1/app/src/main/java/com/example/xptmx/myapp1/Setting.java
@@ -0,0 +1,20 @@
+package com.example.xptmx.myapp1;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.ViewGroup;
+
+
+
+
+public class Setting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle bundle)
+    {
+        super.onCreate(bundle);
+        setContentView(R.layout.disater_message);
+    }
+
+
+}
