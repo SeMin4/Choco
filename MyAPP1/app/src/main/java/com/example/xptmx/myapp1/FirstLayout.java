70ebb6c3daba3264e075cc4d856c936cfa40b198
DESKTOP-OORDLR5\xptmx <xptmxmtest@naver.com>
2018-07-17 20:43:04 +0900

    first version

---
+++ MyAPP1/app/src/main/java/com/example/xptmx/myapp1/FirstLayout.java
@@ -0,0 +1,19 @@
+package com.example.xptmx.myapp1;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FirstLayout extends Fragment {
+    View v;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+      v=inflater.inflate(R.layout.first_layout, container, false);
+      return v;
+    }
+}
