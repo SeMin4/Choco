d43a9afb32ac3e67e0bb56864d866071097dcdf9
SeMin <ohsemin4@gmail.com>
2018-07-18 23:42:52 +0900

    add map navi

--- MyAPP1/app/src/main/java/com/example/xptmx/myapp1/Shelter.java
+++ MyAPP1/app/src/main/java/com/example/xptmx/myapp1/Shelter.java
@@ -1,9 +1,6 @@
 package com.example.xptmx.myapp1;
 
 
-// NMapActivity를 활용해 간단히 지도를 전체화면으로 표시하는 예제
-// 본 예제는 1개의 파일 MainActivity.java 로 구성되어 있습니다.
-// (중요) 패키지명은 애플리케이션 설정의 Android 패키지명과 반드시 일치해야 함
 import android.location.LocationListener;
 import android.os.Bundle;
 import android.util.Log;
@@ -22,38 +19,42 @@ public class Shelter extends NMapActivity {
 
     private NMapView mMapView;// 지도 화면 View
     private final String CLIENT_ID = "aJwTYEg7xKtZ6e15n2ns";// 애플리케이션 클라이언트 아이디 값
-    //private NMapController mMapController;
-    private final String LOG_TAG = "abc" ;
-    private NMapMyLocationOverlay mMapMyLocationOverlay;
-    private NMapLocationManager mMapLocationManager;
-    private NMapCompassManager mMapCompassManager;
-    private NMapOverlayManager mapOverlayManager;
+    private NMapController mMapController;
+    private final String LOG_TAG = "ABC";
     @Override
-    protected void onCreate(Bundle bundle) {
-        super.onCreate(bundle);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    // create map view
         mMapView = new NMapView(this);
+
+    // 기존 API key 방식은 deprecated 함수이며, 2016년 말까지만 사용 가능합니다.
+    // mMapView.setApiKey(API_KEY);
+
+    // set Client ID for Open MapViewer Library
+        mMapView.setClientId(CLIENT_ID);
+
+    // set the activity content to the map view
         setContentView(mMapView);
-   //     mMapLocationManager =new NMapLocationManager(this);
-//        mMapLocationManager.setOnLocationChangeListener(onLocation);
-     //   mMapCompassManager = new NMapCompassManager(this);
-      //  mMapMyLocationOverlay = mapOverlayManager.createMyLocationOverlay(mMapLocationManager,mMapCompassManager);
-        mMapView.setClientId(CLIENT_ID); // 클라이언트 아이디 값 설정
+
+    // initialize map view
         mMapView.setClickable(true);
-        mMapView.setEnabled(true);
-        mMapView.setFocusable(true);
-        mMapView.setFocusableInTouchMode(true);
-        mMapView.requestFocus();
+
+    // register listener for map state changes
+     //   mMapView.setOnMapStateChangeListener(onMapViewStateChangeListener);
+    //    mMapView.setOnMapViewTouchEventListener(onMapViewTouchEventListener);
+
+    // use map controller to zoom in/out, pan and set map center, zoom level etc.
+        mMapController = mMapView.getMapController();
+        mMapView.setBuiltInZoomControls(true, null);
     }
-/*    public void onMapInitHandler(NMapView mapView, NMapError errorInfo) {
+    public void onMapInitHandler(NMapView mapView, NMapError errorInfo) {
         if (errorInfo == null) { // success
-            mMapController.setMapCenter(new
-                    NGeoPoint(126.978371, 37.5666091),15 );
+            mMapController.setMapCenter(new NGeoPoint(126.978371, 37.5666091), 11);
+        } else { // fail
+            Log.e(LOG_TAG, "onMapInitHandler: error=" + errorInfo.toString());
         }
-        else { // fail
-            Log.e(LOG_TAG, "onMapInitHandler: error=" +
-                    errorInfo.toString());
-        }
-    }*/
+    }
 
 
 
