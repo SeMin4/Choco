5388e82141392dd98b24b601778874f3d3c8923e
DESKTOP-OORDLR5\xptmx <xptmxmtest@naver.com>
2018-07-18 20:03:06 +0900

    인트로화면추가버전

---
+++ MyAPP1/app/src/main/java/com/example/xptmx/myapp1/SplashScreenActivity.java
@@ -0,0 +1,37 @@
+package com.example.xptmx.myapp1;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import gr.net.maroulis.library.EasySplashScreen;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle bundle)
+    {
+        super.onCreate(bundle);
+
+        EasySplashScreen config = new EasySplashScreen(SplashScreenActivity.this)
+            .withFullScreen()
+            .withTargetActivity(MainActivity.class)
+            .withSplashTimeOut(1500)
+
+            .withBackgroundColor(Color.parseColor("#322652"))
+            .withHeaderText("")
+            .withFooterText("")
+            .withBeforeLogoText("")
+            .withAfterLogoText("")
+            .withLogo(R.drawable.centerhuman_2);
+
+        config.getHeaderTextView().setTextColor(Color.WHITE);
+        config.getFooterTextView().setTextColor(Color.WHITE);
+        config.getBeforeLogoTextView().setTextColor(Color.WHITE);
+        config.getAfterLogoTextView().setTextColor(Color.WHITE);
+
+        View easySplashScreen = config.create();
+        setContentView(easySplashScreen);
+    }
+}
