42eb6105598d3f153134a0ee1d2f6700ad84d990
SeMin <ohsemin4@gmail.com>
2018-07-19 01:26:48 +0900

    naver map add

--- MyAPP1/app/src/main/java/com/example/xptmx/myapp1/Shelter.java
+++ MyAPP1/app/src/main/java/com/example/xptmx/myapp1/Shelter.java
@@ -1,19 +1,30 @@
 package com.example.xptmx.myapp1;
 
 
+import android.graphics.Rect;
 import android.location.LocationListener;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import com.nhn.android.maps.NMapActivity;
 import com.nhn.android.maps.NMapCompassManager;
 import com.nhn.android.maps.NMapController;
 import com.nhn.android.maps.NMapLocationManager;
+import com.nhn.android.maps.NMapOverlay;
+import com.nhn.android.maps.NMapOverlayItem;
 import com.nhn.android.maps.NMapView;
 import com.nhn.android.maps.maplib.NGeoPoint;
 import com.nhn.android.maps.nmapmodel.NMapError;
+import com.nhn.android.maps.overlay.NMapPOIdata;
+import com.nhn.android.maps.overlay.NMapPOIitem;
+import com.nhn.android.mapviewer.overlay.NMapCalloutCustomOverlay;
+import com.nhn.android.mapviewer.overlay.NMapCalloutOverlay;
 import com.nhn.android.mapviewer.overlay.NMapMyLocationOverlay;
 import com.nhn.android.mapviewer.overlay.NMapOverlayManager;
+import com.nhn.android.mapviewer.overlay.NMapPOIdataOverlay;
+import com.nhn.android.mapviewer.overlay.NMapResourceProvider;
 
 public class Shelter extends NMapActivity {
 
@@ -21,41 +32,192 @@ public class Shelter extends NMapActivity {
     private final String CLIENT_ID = "aJwTYEg7xKtZ6e15n2ns";// 애플리케이션 클라이언트 아이디 값
     private NMapController mMapController;
     private final String LOG_TAG = "ABC";
+    private NMapLocationManager nMapLocationManager;
+    private NGeoPoint My_geoPoint;
+    private NMapCompassManager nMapCompassManager;
+    private NMapOverlayManager mOverlayManager;
+    private NMapMyLocationOverlay mMyLocationOverlay;
+
+    private NMapViewerResourceProvider mMapViewerResourceProvider;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
     // create map view
         mMapView = new NMapView(this);
-
-    // 기존 API key 방식은 deprecated 함수이며, 2016년 말까지만 사용 가능합니다.
-    // mMapView.setApiKey(API_KEY);
-
-    // set Client ID for Open MapViewer Library
-        mMapView.setClientId(CLIENT_ID);
-
-    // set the activity content to the map view
+         mMapView.setClientId(CLIENT_ID);
         setContentView(mMapView);
-
     // initialize map view
         mMapView.setClickable(true);
-
-    // register listener for map state changes
-     //   mMapView.setOnMapStateChangeListener(onMapViewStateChangeListener);
-    //    mMapView.setOnMapViewTouchEventListener(onMapViewTouchEventListener);
-
-    // use map controller to zoom in/out, pan and set map center, zoom level etc.
+        mMapView.setEnabled(true);
+        mMapView.setFocusable(true);
+        mMapView.setFocusableInTouchMode(true);
+        mMapView.requestFocus();
         mMapController = mMapView.getMapController();
         mMapView.setBuiltInZoomControls(true, null);
+        nMapLocationManager = new NMapLocationManager(this);
+        onMapInitHandler(mMapView,null);
+        // create overlay manager
+        mMapViewerResourceProvider = new NMapViewerResourceProvider(this);
+        mOverlayManager = new NMapOverlayManager(this, mMapView, mMapViewerResourceProvider);
+        // register callout overlay listener to customize it.
+        mOverlayManager.setOnCalloutOverlayListener(onCalloutOverlayListener);
+        // register callout overlay view listener to customize it.
+        mOverlayManager.setOnCalloutOverlayViewListener(onCalloutOverlayViewListener);
+        nMapLocationManager = new NMapLocationManager(this);
+        nMapLocationManager.setOnLocationChangeListener(onMyLocationChangeListener);
+        nMapCompassManager = new NMapCompassManager(this);
+        mMyLocationOverlay = mOverlayManager.createMyLocationOverlay(nMapLocationManager, nMapCompassManager);
+
+
+
     }
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStop() {
+
+      //  stopMyLocation();
+
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+
+        // save map view state such as map center position and zoom level.
+      //  saveInstanceState();
+
+        super.onDestroy();
+    }
+
     public void onMapInitHandler(NMapView mapView, NMapError errorInfo) {
         if (errorInfo == null) { // success
-            mMapController.setMapCenter(new NGeoPoint(126.978371, 37.5666091), 11);
+            mMapController.setMapCenter(new NGeoPoint(128.6109119, 35.8883161), 12);
         } else { // fail
             Log.e(LOG_TAG, "onMapInitHandler: error=" + errorInfo.toString());
         }
     }
 
+
+    private final NMapLocationManager.OnLocationChangeListener onMyLocationChangeListener = new NMapLocationManager.OnLocationChangeListener() {
+
+        @Override
+        public boolean onLocationChanged(NMapLocationManager locationManager, NGeoPoint myLocation) {
+
+            if (mMapController != null) {
+                mMapController.animateTo(myLocation);
+            }
+
+            return true;
+        }
+
+        @Override
+        public void onLocationUpdateTimeout(NMapLocationManager locationManager) {
+
+            // stop location updating
+            //			Runnable runnable = new Runnable() {
+            //				public void run() {
+            //					stopMyLocation();
+            //				}
+            //			};
+            //			runnable.run();
+
+            Toast.makeText(Shelter.this, "", Toast.LENGTH_SHORT).show();
+            Toast.makeText(Shelter.this, "Your current location is temporarily unavailable.", Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void onLocationUnavailableArea(NMapLocationManager locationManager, NGeoPoint myLocation) {
+
+            Toast.makeText(Shelter.this, "Your current location is unavailable area.", Toast.LENGTH_LONG).show();
+
+
+        }
+
+    };
+    private final NMapOverlayManager.OnCalloutOverlayListener onCalloutOverlayListener = new NMapOverlayManager.OnCalloutOverlayListener() {
+
+        @Override
+        public NMapCalloutOverlay onCreateCalloutOverlay(NMapOverlay itemOverlay, NMapOverlayItem overlayItem,
+                                                         Rect itemBounds) {
+
+            // handle overlapped items
+            if (itemOverlay instanceof NMapPOIdataOverlay) {
+                NMapPOIdataOverlay poiDataOverlay = (NMapPOIdataOverlay)itemOverlay;
+
+                // check if it is selected by touch event
+                if (!poiDataOverlay.isFocusedBySelectItem()) {
+                    int countOfOverlappedItems = 1;
+
+                    NMapPOIdata poiData = poiDataOverlay.getPOIdata();
+                    for (int i = 0; i < poiData.count(); i++) {
+                        NMapPOIitem poiItem = poiData.getPOIitem(i);
+
+                        // skip selected item
+                        if (poiItem == overlayItem) {
+                            continue;
+                        }
+
+                        // check if overlapped or not
+                        if (Rect.intersects(poiItem.getBoundsInScreen(), overlayItem.getBoundsInScreen())) {
+                            countOfOverlappedItems++;
+                        }
+                    }
+
+                    if (countOfOverlappedItems > 1) {
+                        String text = countOfOverlappedItems + " overlapped items for " + overlayItem.getTitle();
+                        Toast.makeText(Shelter.this, text, Toast.LENGTH_LONG).show();
+                        return null;
+                    }
+                }
+            }
+
+            // use custom old callout overlay
+            if (overlayItem instanceof NMapPOIitem) {
+                NMapPOIitem poiItem = (NMapPOIitem)overlayItem;
+
+                if (poiItem.showRightButton()) {
+                    return new NMapCalloutCustomOldOverlay(itemOverlay, overlayItem, itemBounds,
+                            mMapViewerResourceProvider);
+                }
+            }
+
+            // use custom callout overlay
+            return new NMapCalloutCustomOverlay(itemOverlay, overlayItem, itemBounds, mMapViewerResourceProvider);
+
+            // set basic callout overlay
+            //return new NMapCalloutBasicOverlay(itemOverlay, overlayItem, itemBounds);
+        }
+
+    };
+    private final NMapOverlayManager.OnCalloutOverlayViewListener onCalloutOverlayViewListener = new NMapOverlayManager.OnCalloutOverlayViewListener() {
+
+        @Override
+        public View onCreateCalloutOverlayView(NMapOverlay itemOverlay, NMapOverlayItem overlayItem, Rect itemBounds) {
+
+            if (overlayItem != null) {
+                // [TEST] 말풍선 오버레이를 뷰로 설정함
+                String title = overlayItem.getTitle();
+                if (title != null && title.length() > 5) {
+                    return new NMapCalloutCustomOverlayView(Shelter.this, itemOverlay, overlayItem, itemBounds);
+                }
+            }
+
+            // null을 반환하면 말풍선 오버레이를 표시하지 않음
+            return null;
+        }
+
+    };
 
 
 
