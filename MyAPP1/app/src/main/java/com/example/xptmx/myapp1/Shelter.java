aa46423f9aea030b2c1f9c8d465462b27818afe8
SeMin <ohsemin4@gmail.com>
2018-07-18 14:13:58 +0900

    v.0.0.1

--- MyAPP1/app/src/main/java/com/example/xptmx/myapp1/Shelter.java
+++ MyAPP1/app/src/main/java/com/example/xptmx/myapp1/Shelter.java
@@ -4,18 +4,30 @@ package com.example.xptmx.myapp1;
 // 본 예제는 1개의 파일 MainActivity.java 로 구성되어 있습니다.
 // (중요) 패키지명은 애플리케이션 설정의 Android 패키지명과 반드시 일치해야 함
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
 import com.nhn.android.maps.NMapActivity;
+import com.nhn.android.maps.NMapCompassManager;
+import com.nhn.android.maps.NMapLocationManager;
 import com.nhn.android.maps.NMapView;
+import com.nhn.android.maps.maplib.NGeoPoint;
+import com.nhn.android.maps.nmapmodel.NMapError;
+import com.nhn.android.mapviewer.overlay.NMapMyLocationOverlay;
+import com.nhn.android.mapviewer.overlay.NMapOverlayManager;
 
 public class Shelter extends NMapActivity {
 
     private NMapView mMapView;// 지도 화면 View
     private final String CLIENT_ID = "aJwTYEg7xKtZ6e15n2ns";// 애플리케이션 클라이언트 아이디 값
 
+
     @Override
     protected void onCreate(Bundle bundle) {
         super.onCreate(bundle);
         mMapView = new NMapView(this);
+
+
         setContentView(mMapView);
         mMapView.setClientId(CLIENT_ID); // 클라이언트 아이디 값 설정
         mMapView.setClickable(true);
@@ -23,7 +35,11 @@ public class Shelter extends NMapActivity {
         mMapView.setFocusable(true);
         mMapView.setFocusableInTouchMode(true);
         mMapView.requestFocus();
+
+
     }
+
+
 }
 
 
