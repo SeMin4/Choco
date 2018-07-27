650613f7128211a63a2bdec34e86f087a8b89d3f
SeMin <ohsemin4@gmail.com>
2018-07-27 14:39:33 +0900

    add.v

--- MyAPP1/app/src/main/java/com/example/xptmx/myapp1/Tips.java
+++ MyAPP1/app/src/main/java/com/example/xptmx/myapp1/Tips.java
@@ -1,13 +1,15 @@
 package com.example.xptmx.myapp1;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class Tips extends AppCompatActivity{
+public class Tips extends AppCompatActivity {
     @Override
-    protected void onCreate(Bundle bundle)
-    {
+    protected void onCreate(Bundle bundle) {
         super.onCreate(bundle);
         setContentView(R.layout.tips);
     }
+
+
 }
